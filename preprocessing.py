ceb712f58b2faf5623b298dc535fdd759ad18db5
Lojze Žust <lojze.zust@gmail.com>
2020-05-24 16:47:07 +0200

    TokenDictionary fix

--- preprocessing.py
+++ preprocessing.py
@@ -262,7 +262,7 @@ class TokenDictionary():
         self.token_map = {token: i for i, token in enumerate(token_dictionary)}
 
         # Compute idf for each word
-        bow = np.stack([self.bag_of_words(document) for document in documents])
+        bow = np.stack([self.bag_of_words(document, include_ood=True) for document in documents])
         num_documents = len(documents)
         word_occurences = np.sum(bow>0, axis=0)
 
