16fcd1f2b3d2aeef87da4f80aad50cefc4e471b9
Lojze Žust <lojze.zust@gmail.com>
2020-05-24 20:49:16 +0200

    Replaced classification

--- classification.py
+++ classification.py
@@ -30,181 +30,39 @@ import pycrfsuite
 from baseline import evaluate_solution
 from csv_parser import split_train_test
 from confusion_matrix_pretty_print import pretty_plot_confusion_matrix
+from evaluation import Evaluator
 
+import features as F
+from classification import build_ngram_model
+from models import MajorityModel, SklearnModel, CRFModel
+from cross_validation import cross_validate
 
-# 1. Define feature functions
-
-# Only length as a feature
-def get_features_length(message_i):
-    message = df.loc[message_i]['Message']
-    feature_list = []
-
-    count = len(message.split())
-    feature_list.append(count)
-
-    return feature_list
-
-
-# BoW as features
-def get_features_bow(message_i, bow_values):
-    #     message = df.loc[message_i]
-    message_bow = bow_values[message_i]
-
-    features = []
-    for i, w in enumerate(message_bow):
-        features.append(w)
-
-    return features
-
-
-def get_features_context(message_i, books_context, count=False, data_type='Book ID'):
-    book_id = df.loc[message_i][data_type]
-    if book_id not in books_context.keys():
-        return [0]
-
-    book_tokens = books_context[book_id]
-    message_tokens = messages[message_i]
-
-    if not count:
-        return [sum([1 if token in book_tokens else 0 for token in message_tokens])]
-
-    count = 0
-    for token in book_tokens:
-        if token in message_tokens:
-            count += 1
-    return [count]
-
-
-def get_cosine_distance(message_i, bow_values, bow_books, data_type='Book ID'):
-    book_id = df.loc[message_i][data_type]
-    if book_id not in bow_books.keys():
-        return [1]
-
-    message_bow = bow_values[message_i]
-    book_bow = bow_books[book_id]
-
-    if sum(message_bow) == 0:
-        return [1]
-
-    return [1 - spatial.distance.cosine(message_bow, book_bow)]
-
-
-def get_tfidf_from_book(message_i, bow_values, bow_books, data_type='Book ID'):
-    book_id = df.loc[message_i][data_type]
-    if book_id not in bow_books.keys():
-        return [0]
-
-    message_bow = bow_values[message_i]
-    book_bow = bow_books[book_id]
-
-    return [np.dot(message_bow, book_bow)]
-
-
-def get_features_rawlength(message_i):
-    message = df.loc[message_i]['Message']
-    feature_list = []
-
-    count = len(message)
-    feature_list.append(count)
-
-    return feature_list
-
-
-def get_features_previous_msg(message_i, bow_values):
-    tokens = bow_values[message_i]
-    tokens_p = bow_values[message_i - 1]
-
-    if not sum(tokens) or not sum(tokens_p):
-        return [0]
-
-    return [np.dot(tokens, tokens_p)]
-
-# Sentiment
-def get_features_sent(message_id):
-    message = messages_sent[message_id]
-    sent = sa.sentiment(message)
-
-    features = {'sentiment': sent}
-
-    return features.values()
-
-def get_features_history(message_id):
-    entry = df.loc[message_id]
-    time = entry['Message Time']
-    username = entry['Name']
-
-    conversation = df[df['School'] == entry['School']]
-    conversation = conversation[conversation['Bookclub'] == entry['Bookclub']]
-    conversation = conversation[conversation['Topic'] == entry['Topic']]
-
-    time_mask = (conversation['Message Time'] < time) & (conversation['Message Time'] > time - timedelta(minutes=5))
-    username_mask = conversation['Name'] == username
-
-    n_last_5min = len(conversation[time_mask & username_mask])
-    n_posts_5min = len(conversation[time_mask])
-    n_users_5min = len(conversation[time_mask]['Name'].unique())
-
-    features = {
-        'recent_user_posts': n_last_5min,
-        'recent_posts': n_posts_5min,
-        'recent_users': n_users_5min
-    }
-
-    return features.values()
-
-
-def get_label(message_i):
-    message = df.loc[message_i]
-    return message['CategoryBroad']
-
-
-def conversation2features(conversation, feature_fn):
-    features = [feature_fn(msg_i) for msg_i in conversation]
-
-    return features
-
-
-def conversation2labels(conversation, labels_fn):
-    labels = [labels_fn(msg_i) for msg_i in conversation]
-    return labels
-
-
-def build_ngram_model(messages, n=2):
-    ngram_model = []
-    for msg in messages:
-        ngram_model.append(list(ngrams('_'.join(msg), n)))
-
-    return ngram_model
-
-
-if __name__ == "__main__":
+if __name__=='__main__':
+    # Read data
     dataset_path = 'data/discussion_data.csv'
     df = read_dataset(dataset_path)
-
     df['Message Time'] = pd.to_datetime(df['Message Time'])
+    tags = df.CategoryBroad.unique()
 
-    # data scaler
-    # scaler = MinMaxScaler()
-    # scaler = MaxAbsScaler()
-    scaler = Normalizer()
-
+    # Tokenization, lemmatization, roof removal
     tokenizer = Tokenization()
     stop_words_remover = StopWordsRemover('data/stopwords-sl-custom.txt')
     lemmatizer = Lemmatization()
-
     roof_removal = RoofRemoval()
-    spelling_correction = SpellingCorrection('data/dict-sl.txt', roof_removal)
 
-    # sentiment analysis
-    sa = SentimentAnalysis('data/negative_words_Slolex.txt', 'data/positive_words_Slolex.txt', roof_removal)
+    # spelling_correction = SpellingCorrection('data/dict-sl.txt', roof_removal)
 
+    # Gibberish detector
     gibberish_detector = GibberishDetector(roof_removal)
-    # Train gibberish_detector
     gibberish_detector.train('data/dict-sl.txt', 'data/gibberish_good.txt', 'data/gibberish_bad.txt')
 
+    # Token grouping
     token_grouping = TokenGrouping(gibberish_detector)
 
-    # tokenize the books
+    sa = SentimentAnalysis('data/negative_words_Slolex.txt', 'data/positive_words_Slolex.txt', roof_removal)
+
+
+    # Tokenize the books
     book_tokens = {}
     for book in os.listdir('data/books'):
         book_id = int(book.split('.')[0])
@@ -219,7 +77,7 @@ if __name__ == "__main__":
             tokens = [token for token in tokens if token.isalpha() and not token == '<other>']
             book_tokens[book_id] = tokens
 
-    # tokenize the topics
+    # Tokenize the topics
     topics_tokens = {}
     for topic in df['Topic'].unique():
         tokens = tokenizer.tokenize(topic)
@@ -231,141 +89,99 @@ if __name__ == "__main__":
         tokens = [token for token in tokens if token.isalpha() and not token == '<other>']
         topics_tokens[topic] = tokens
 
-    # Tokenization
-    messages = df.Message
-    messages = [tokenizer.tokenize(message) for message in messages]
+    # Tokenize the messages
+    messages_sent = []
+    for message in df.Message:
+        tokens = tokenizer.tokenize(message)
+        tokens = stop_words_remover.remove_stopwords(tokens)
+        messages_sent.append(tokens)
 
-    # Remove stop words
-    messages = [stop_words_remover.remove_stopwords(tokens) for tokens in messages]
-    messages_sent = messages
-
-    # Lemmatization
-    messages = [[lemmatizer.lemmatize(token) for token in message] for message in messages]
-
-    # Roof removal
-    messages = [[roof_removal.remove(token) for token in message] for message in messages]
-
-    # Token grouping
-    messages = [[token_grouping.group_tokens(token) for token in message] for message in messages]
+    messages = []
+    for tokens in messages_sent:
+        tokens = [lemmatizer.lemmatize(token) for token in tokens]
+        tokens = [roof_removal.remove(token) for token in tokens]
+        tokens = [token_grouping.group_tokens(token) for token in tokens]
+        messages.append(tokens)
 
     # Create BoW dictionary
-    token_dict = TokenDictionary(messages +
-                                 [cont for cont in book_tokens.values()] +
-                                 [cont for cont in topics_tokens.values()], dict_size=1024)
+    token_dict_msg = TokenDictionary(messages, dict_size=512)
+    token_dict_context = TokenDictionary(list(book_tokens.values()) + list(topics_tokens.values()), dict_size=512)
 
     # Create BoW of ngrams
     ngram_arr = build_ngram_model([msg.split() for msg in df.Message], 2)
     ngram_dict = TokenDictionary(ngram_arr, dict_size=1024)
 
     # Get tf-idf weighted BoW representations
-    bow = np.stack([token_dict.bag_of_words(message) for message in messages])
-    bow_tfidf = np.stack([token_dict.bag_of_words(message, tf_idf=True) for message in messages])
-    book_bow = dict([(key, token_dict.bag_of_words(value)) for (key, value) in book_tokens.items()])
-    book_tfidf = dict([(key, token_dict.bag_of_words(value, tf_idf=True)) for (key, value) in book_tokens.items()])
-    topic_bow = dict([(key, token_dict.bag_of_words(value)) for (key, value) in topics_tokens.items()])
-    topic_tfidf = dict([(key, token_dict.bag_of_words(value, tf_idf=True)) for (key, value) in topics_tokens.items()])
+    bow = np.stack([token_dict_msg.bag_of_words(message) for message in messages])
+    bow_tfidf = np.stack([token_dict_msg.bag_of_words(message, tf_idf=True) for message in messages])
+
+    context_bow = np.stack([token_dict_context.bag_of_words(message) for message in messages])
+    context_bow_tfidf = np.stack([token_dict_context.bag_of_words(message, tf_idf=True) for message in messages])
 
     # Get ngram model
-    ngram_model = np.stack([ngram_dict.bag_of_words(gram) for gram in ngram_arr])
-
-    mask = df['Book ID'].isin(book_tokens.keys())
-    df = df[mask]
+    ngram_model = np.stack([ngram_dict.bag_of_words(ngram) for ngram in ngram_arr])
 
     kFolds = split_train_test(df)
 
-    combine_matrix_test = np.zeros([len(list(df.CategoryBroad.unique())), len(list(df.CategoryBroad.unique()))],
-                                   dtype=np.uint32)
-    combine_matrix_train = np.zeros([len(list(df.CategoryBroad.unique())), len(list(df.CategoryBroad.unique()))],
-                                    dtype=np.uint32)
+    # Create models
+    model1 = SklearnModel(LinearSVC(max_iter=4000))
+    model2 = SklearnModel(RandomForestClassifier(n_estimators=500))
+    model3 = CRFModel('test')
 
-    for (train_dfs, test_dfs) in kFolds:
-        print(test_dfs[0]['School'].unique())
-        train_dfs_all = pd.concat(train_dfs)
-        test_dfs_all = pd.concat(test_dfs)
-        majority_class = train_dfs_all.CategoryBroad.value_counts().index[0]
+    # Create base feature functions
+    fn_length = functools.partial(F.length, df=df)
+    fn_wordcount = functools.partial(F.wordcount, df=df)
+    fn_recent_activity = functools.partial(F.recent_activity, df=df)
+    fn_sentiment = functools.partial(F.sentiment, messages=messages_sent, sentiment_analysis=sa, normalize=True)
 
-        conversation_list_train = list(train_dfs_all.index)
-        conversation_list_test = list(test_dfs_all.index)
+    fn_msg_bow = functools.partial(F.message_bow, bow_values=bow_tfidf)
+    fn_ngram_bow = functools.partial(F.message_bow, bow_values=ngram_model)
+    fn_context_bow = functools.partial(F.message_bow, bow_values=context_bow_tfidf)
 
-        features_fns = []
-        # length features
-        features_fns.append(get_features_length)
-        features_fns.append(get_features_rawlength)
 
-        # previous msg similarity
-        features_fns.append(functools.partial(get_features_previous_msg, bow_values=bow))
+    # Create feature functions combinations
+    feature_fn1 = F.merge_feature_functions([
+        fn_length,
+        fn_wordcount,
+        fn_sentiment,
+        fn_recent_activity
+    ])
 
-        # history features
-        features_fns.append(get_features_history)
+    feature_fn2 = F.merge_feature_functions([
+        fn_msg_bow,
+        fn_ngram_bow
+    ])
 
-        # features sentiment
-        features_fns.append(get_features_sent)
+    feature_fn3 = F.merge_feature_functions([
+        fn_length,
+        fn_wordcount,
+        fn_msg_bow,
+        fn_ngram_bow,
+        fn_sentiment,
+        fn_recent_activity,
+        fn_context_bow
+    ])
 
-        # BoW features
-        # features_fns.append(functools.partial(get_features_bow, bow_values=bow))
-        features_fns.append(functools.partial(get_features_bow, bow_values=bow_tfidf))
+    labels_fn = functools.partial(F.get_label, df=df)
 
-        # nGram model
-        features_fns.append(functools.partial(get_features_bow, bow_values=ngram_model))
 
-        # similarity to the book
-        # features_fns.append(functools.partial(get_features_context, books_context=book_tokens, count=True))
-        # features_fns.append(functools.partial(get_cosine_distance, bow_values=bow_tfidf, bow_books=book_tfidf))
-        # features_fns.append(functools.partial(get_tfidf_from_book, bow_values=bow, bow_books=book_tfidf))
+    models = [model1, model2]
+    features_fns = [feature_fn1, feature_fn2, feature_fn3]
 
-        # similarity to the topic
-        # features_fns.append(functools.partial(get_features_context, books_context=topics_tokens, count=True, data_type='Topic'))
-        # features_fns.append(functools.partial(get_cosine_distance, bow_values=bow, bow_books=topic_bow, data_type='Topic'))
-        # features_fns.append(functools.partial(get_tfidf_from_book, bow_values=bow, bow_books=topic_tfidf, data_type='Topic'))
+    for i, features_fn in enumerate(features_fns):
+        for j, model in enumerate(models):
+            print(f"Model {j}, feature set {i}")
+            res = cross_validate(kFolds, model, features_fn, labels_fn)
+            train_preds, train_labels, test_preds, test_labels = res
 
-        # get labels for results
-        labels_fn = get_label
+            test_evaluator = Evaluator(test_preds, test_labels, tags)
+            test_evaluator.get_classification_report(plot=True)
+            train_evaluator = Evaluator(train_preds, train_labels, tags)
+            train_evaluator.get_classification_report(plot=True)
 
-        X_train = [list(chain.from_iterable([features_fn(s) for features_fn in features_fns])) for s in conversation_list_train]
-        y_train = [labels_fn(s) for s in conversation_list_train]
+            # For example: accuracy
+            train_acc = np.mean(np.array(train_preds) == np.array(train_labels))
+            test_acc = np.mean(np.array(test_preds) == np.array(test_labels))
 
-        X_test = [list(chain.from_iterable([features_fn(s) for features_fn in features_fns])) for s in conversation_list_test]
-        y_test = [labels_fn(s) for s in conversation_list_test]
-
-        # normalize features
-        # scaler.fit_transform(X_train)
-        # X_train = scaler.transform(X_train)
-        # X_test = scaler.transform(X_test)
-
-        # clf = RandomForestClassifier(n_estimators=500)
-        clf = GaussianNB()
-        # clf = DecisionTreeClassifier()
-        # clf = KNeighborsClassifier(metric='minkowski', n_neighbors=9)
-        # clf = LinearSVC(max_iter=4000)
-        # clf = SVC(max_iter=1000)
-        # clf = MLPClassifier()
-
-        clf.fit(X_train, y_train)
-
-        preds = clf.predict(X_test)
-        labels = [tag for convo in y_test for tag in convo]
-        tags = list(df.CategoryBroad.unique())
-
-        conf_mat = evaluate_solution(preds, labels, tags, majority_class=majority_class, verbose=False)
-        combine_matrix_test = combine_matrix_test + conf_mat
-
-        preds = clf.predict(X_train)
-        labels = [tag for convo in y_train for tag in convo]
-        tags = list(df.CategoryBroad.unique())
-
-        conf_mat = evaluate_solution(preds, labels, tags, majority_class=majority_class, verbose=False)
-        combine_matrix_train = combine_matrix_train + conf_mat
-
-    print("Test results ======================")
-    print(combine_matrix_test)
-    print(
-        f'Combined CA: {sum([combine_matrix_test[i][i] for i in range(len(combine_matrix_test))]) / np.sum(combine_matrix_test)}')
-    pretty_plot_confusion_matrix(pd.DataFrame(combine_matrix_test, columns=tags, index=tags), cmap="Blues")
-
-    print("Train results ======================")
-    print(combine_matrix_train)
-    print(
-        f'Combined CA: {sum([combine_matrix_train[i][i] for i in range(len(combine_matrix_train))]) / np.sum(combine_matrix_train)}')
-    pretty_plot_confusion_matrix(pd.DataFrame(combine_matrix_train, columns=tags, index=tags), cmap="Blues")
-
-    plt.show()
+            print(f'Train accuracy: {train_acc}')
+            print(f'Test accuracy: {test_acc}')
